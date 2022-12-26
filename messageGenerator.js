922e87e3e72e23fa3461d4061e6df50f26d4251d
rafaelRueda <rrrm85@gmail.com>
2022-12-26 10:29:34 -0500

    Adding messageGenerator

---
+++ messageGenerator.js
@@ -0,0 +1,7 @@
+//Console log a random message every time the program is run
+
+
+//Log three different messages, wich part are the same and which are differnt
+ //example: The chinise sing is: ****, characteristics is ** and luky color ***
+
+ console.log(`The chinise sing is: ****, characteristics is ** and luky color ***`); 
