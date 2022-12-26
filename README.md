2ede4a7477510688f16e1d93b6fbc9f63f6eb9eb
rafaelRueda <rrrm85@gmail.com>
2022-12-26 10:59:57 -0500

    Version 1 of the program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Message Generator
 
+##Use
+This program log a ramdon messages whit a chinese zodiac sign, a personality traid and a lucky color 
--- messageGenerator.js
+++ messageGenerator.js
@@ -1,7 +1,61 @@
 //Console log a random message every time the program is run
 
+ //Store data for the messages
+ const sings =[
+    {
+        sign: "Rat",
+        personality: ['quick-witted', 'resourceful', 'versatile'] 
+    },
+    {
+        sign: "Ox",
+        personality: ['diligent', 'dependable', 'determined']
+    },
+    {
+        sign: "Tiger",
+        personality: ['brave', 'competitive', 'unpredictable']
+    },
+    {
+        sign: "Rabbit",
+        personality: ['quiet', 'elegant', 'kind']
+    },
+    {
+        sign: "Dragon",
+        personality: ['confident', 'intelligent', 'wise']
+    },
+    {
+        sign: "Snake",
+        personality: ['enigmatic', 'intelligent', 'wise']
+    },
+    {
+        sign: "Horse",
+        personality: ['animated', 'active', 'energetic']
+    },
+    {
+        sign: "Goat",
+        personality: ['calm', 'gentle', 'sympathetic']
+    },
+    {
+        sign: "Monkey",
+        personality: ['sharp', 'smart', 'curious',]
+    },
+    {
+        sign: "Rooster",
+        personality: ['observant', 'hardworking', 'courageous']
+    },
+    {
+        sign: "Dog",
+        personality: ['lovely', 'honest', 'prudent']
+    },
+    {
+        sign: "Pig",
+        personality: ['compassionate', 'generous', 'strong']
+    }
+ ];
 
-//Log three different messages, wich part are the same and which are differnt
- //example: The chinise sing is: ****, characteristics is ** and luky color ***
+ const color = ['blue', 'red', 'black', 'yellow', 'green', 'gold', 'oragen', 'white', 'brown', 'purple'];
 
- console.log(`The chinise sing is: ****, characteristics is ** and luky color ***`); 
+ let randomSign = Math.floor(Math.random() * 12);
+ let randomPersonality = Math.floor(Math.random() * 3);
+ let randomColor = Math.floor(Math.random() * 10);
+
+ console.log(`Your chinise sing is: ${sings[randomSign].sign}, characteristics is ${sings[randomSign].personality[randomPersonality]} and luky color: ${color[randomColor]}`); 
